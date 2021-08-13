e083de0fa70ea87aedd9b6ed3af390eceb6b2e5a
Parinthorn Kanurak <iken.oops@hotmail.com>
2021-08-13 10:30:20 +0700

    Add project files.

---
+++ method2 week2/Source.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void sum(float x[]) {
+	printf("%f + %f = %f\n", x[0], x[1], x[0] + x[1]);
+}
+void minus(float x[]) {
+	printf("%f - %f = %f\n", x[0], x[1], x[0] - x[1]);
+}
+void multiple(float x[]) {
+	printf("%f * %f = %f\n", x[0], x[1], x[0] * x[1]);
+}
+void divide(float x[]) {
+	printf("%f +/%f = %f\n", x[0], x[1], x[0] / x[1]);
+}
+int main() {
+
+	float x[2];
+	printf("enter the number :");
+	scanf_s("%f", &x[0]);
+	printf("enter another number : ");
+	scanf_s("%f", &x[1]);
+	sum(x);
+	minus(x);
+	multiple(x);
+	divide(x);
+
+	return 0;
+}
+
